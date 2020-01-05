4c337864a3699e8f7ab698f4d26c738329370502
Gangadharamm <59532289+Gangadharamm@users.noreply.github.com>
2020-01-05 21:23:26 +0530

    Create Polindrome.java

---
+++ Polindrome.java
@@ -0,0 +1,20 @@
+package com.mgr;
+/**
+ * 
+ */
+
+/**
+ * @author Gangadharam
+ *
+ */
+public class Polindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
